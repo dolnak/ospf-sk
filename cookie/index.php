cad99e6ce2b92252b77b35f3ac5cf220c12df7de
Ivan Dolnák <ivan.dolnak@connet-group.sk>
2018-08-22 14:43:53 +0200

    Fixed bugs

--- cookie/index.php
+++ cookie/index.php
@@ -5,7 +5,7 @@ $lang = 'sk';
 $title = 'Zásady používania súborov cookie';
 $description = 'Na analýzu náštevnosti tohto webu používame súbory cookie. Zoznámte sa so zásadami používania súborov cookie na našom webe.';
 $h1 = 'Zásady používania súborov cookie';
-$p_lead = 'podľa <a href="https://www.zakonypreludi.sk/zz/2011-351#p55">§&nbsp;55 Zákona o&nbsp;elektronických komunikáciách</a>';
+$p_lead = 'podľa §&nbsp;55 Zákona o&nbsp;elektronických komunikáciách';
 
 $content = <<<MAIN
 
@@ -15,28 +15,28 @@ $content = <<<MAIN
     <p class="lead">$p_lead</p>
   </div>
 </div>
-<div class="container">
-  <p class="text-justify">
-    V&nbsp;súlade s §&nbsp;55 ods.&nbsp;5 Zákona č.&nbsp;351/2011 Z.&nbsp;z. o&nbsp;elektronických komunikáciách v&nbsp;znení neskorších predpisov si vás dovoľujeme informovať o&nbsp;používaní súborov cookie a&nbsp;upriamiť vašu pozornosť na možnosť zmeny nastavenia vášho internetového prehliadača pre prípad, že vám aktuálne nastavenie využívania súborov cookie nevyhovuje.
+<div class="container text-justify">
+  <p>
+    V&nbsp;súlade s <a href="https://www.zakonypreludi.sk/zz/2011-351#p55">§&nbsp;55 ods.&nbsp;5 Zákona č.&nbsp;351/2011 Z.&nbsp;z. o&nbsp;elektronických komunikáciách</a> v&nbsp;znení neskorších predpisov si vás dovoľujeme informovať o&nbsp;používaní súborov cookie a&nbsp;upriamiť vašu pozornosť na možnosť zmeny nastavenia vášho internetového prehliadača pre prípad, že vám aktuálne nastavenie využívania súborov cookie nevyhovuje.
   </p>
   <h2>Čo sú súbory cookie?</h2>
-  <p class="text-justify">
+  <p>
     Súbory cookie sú malé textové súbory, ktoré môžu byť do internetového prehliadača odosielané pri návšteve webových stránok a&nbsp;ukladané do vášho zariadenia (počítača alebo do iného zariadenia s&nbsp;prístupom na internet, ako napr. smartphone alebo tablet). Súbory cookie sa ukladajú do priečinka pre súbory vášho internetového prehliadača. Súbory cookie obvykle obsahujú názov webovej stránky, z&nbsp;ktorej pochádzajú a&nbsp;dátum svojho vzniku. Pri ďalšej návšteve stránky webový prehliadač znovu načíta súbory cookie a&nbsp;tieto informácie odošle späť webovej stránke, ktorá pôvodne súbory cookie vytvorila. Súbory cookie, ktoré používame, nepoškodzujú váš počítač.
   </p>
   <h2>Používanie súborov cookie</h2>
-  <p class="text-justify">
+  <p>
     Používaním týchto stránok vyjadrujete súhlas s&nbsp;použitím súborov cookie v&nbsp;súlade s&nbsp;nastavením vášho internetového prehliadača. Ak navštívite naše webové stránky, v&nbsp;prehliadači máte povolené prijímanie súborov cookie, nevykonáte zmenu nastavení vášho internetového prehliadača a&nbsp;pokračujete v&nbsp;návšteve našich webových stránok, považujeme to za prijatie našich podmienok používania súborov cookie.
   </p>
   <h2>Prečo používame súbory cookie?</h2>
-  <p class="text-justify">
+  <p>
     Súbory cookie používame s&nbsp;cieľom optimálne vytvárať a&nbsp;neustále skvalitňovať naše služby, prispôsobiť ich vašim záujmom a&nbsp;potrebám a&nbsp;zlepšovať ich štruktúru a&nbsp;obsah, ako aj na vytváranie zaujímavých ponúk pre vás. Nepoužívame údaje získané používaním súborov cookie ako kontaktné údaje na kontaktovanie vás prostredníctvom pošty, elektronickej pošty alebo telefónu. Súbory cookie umožňujú zobrazovať cielenú reklamu na našich webových stránkach a&nbsp;iných webových stránkach na internete, ktoré sú zapojené do reklamného systému. Cielená reklama je zobrazená na základe správania používateľa či skupín používateľov na webových stránkach, vďaka identifikácii ich prehliadača.
   </p>
   <h2>Ako môžete nastavenia súborov cookie zmeniť?</h2>
-  <p class="text-justify">
+  <p>
     Väčšina internetových prehliadačov je pôvodne nastavená na automatické akceptovanie súborov cookie. Toto nastavenie môžete zmeniť zablokovaním súborov cookie alebo upozornením v&nbsp;prípade, že sa majú súbory cookie poslať do vášho zariadenia. Inštrukcie na zmenu nastavenia súborov cookie nájdete vo voľbe „Pomocník“ každého prehliadača. Ak používate rozličné zariadenia na prístup k&nbsp;stránkam (napr. počítač, smartphone, tablet), odporúčame každý prehliadač na každom zariadení prispôsobiť vašim preferenciám súborov cookie.
   </p>
   <h2>Prečo si ponechať nastavenie súborov cookie?</h2>
-  <p class="text-justify">
+  <p>
     Používanie súborov cookie a&nbsp;ich povolenie vo webovom prehliadači je na vašom rozhodnutí. V&nbsp;prípade zmeny ich nastavenia však môžu mať niektoré naše webové stránky obmedzenú funkčnosť a&nbsp;znížený používateľský komfort.
   </p>
 </div>
--- lorem-ipsum/index.php
+++ lorem-ipsum/index.php
@@ -15,29 +15,29 @@ $content = <<<MAIN
     <p class="lead">$p_lead</p>
   </div>
 </div>
-<div class="container">
+<div class="container text-justify">
   <h2>Lorem ipsum dolor sit amet</h2>
-  <p class="text-justify">
+  <p>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac consequat mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin ultricies erat nec sem aliquam iaculis. Ut ex enim, tempus eget tellus eget, vehicula vestibulum nisi. Aenean consectetur hendrerit velit, vel blandit mauris tempus id. Suspendisse elit erat, suscipit sed eros id, dapibus iaculis nulla. Donec vitae enim arcu. Ut interdum placerat orci vitae molestie. Aliquam vel venenatis odio. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce id mattis arcu. Donec elit orci, malesuada faucibus velit vel, sodales accumsan nisl. Praesent gravida tincidunt turpis. In non lorem a diam hendrerit placerat id non enim.
   </p>
   <div class="container">
     <h3>Cras semper dolor magna</h3>
-    <p class="text-justify">
+    <p>
       Cras semper dolor magna, ac placerat diam egestas sed. Quisque elementum mi et porta sagittis. In vehicula molestie lacus sed luctus. Fusce faucibus posuere laoreet. Phasellus ut nibh nisi. Duis imperdiet suscipit purus, a convallis sapien varius in. Nulla sagittis ligula sed nibh lacinia euismod scelerisque at arcu. Maecenas dui dui, hendrerit aliquam hendrerit id, porta vitae tellus. Integer scelerisque enim ac molestie tincidunt. In massa ipsum, feugiat vitae enim quis, porta semper elit. Cras congue velit sed ultricies cursus.
     </p>
     <div class="container">
       <h4>Aenean volutpat arcu et erat varius condimentum</h4>
-      <p class="text-justify">
+      <p>
         Aenean volutpat arcu et erat varius condimentum. Phasellus rhoncus arcu vel nibh mollis, sed vehicula erat facilisis. Fusce ut sem eget quam mollis bibendum. Cras varius nec lacus sit amet aliquam. Duis lorem felis, fermentum vel nulla a, scelerisque vulputate mi. Praesent id vulputate nunc, at malesuada ipsum. Phasellus a commodo ipsum. Integer lacus ante, sagittis nec enim sit amet, rutrum rutrum arcu. Duis porta massa at dui aliquet, quis ultrices arcu efficitur.
       </p>
     </div>
   </div>
   <h2>Orci varius natoque penatibus et magnis dis parturient montes</h2>
-  <p class="text-justify">
+  <p>
     Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris hendrerit tortor ut accumsan accumsan. Cras nec dapibus augue. Proin at mollis nisi. Suspendisse eget felis nisl. Morbi ullamcorper nisl tellus, sit amet eleifend turpis fringilla cursus. Nullam dapibus magna at nisi pellentesque, eget tristique nulla finibus. Donec blandit, arcu bibendum tristique volutpat, sem elit convallis sem, in molestie ante mi vel sem. Curabitur orci metus, elementum non pellentesque nec, euismod eget velit. In posuere enim eget nisi tempor lacinia. Donec vulputate at sem in tempus. Donec ornare velit non eros efficitur aliquet. Fusce bibendum nulla vitae metus consequat, vitae porttitor neque commodo.
   </p>
   <h2>Nullam pulvinar tristique lectus</h2>
-  <p class="text-justify">
+  <p>
     Nullam pulvinar tristique lectus, in pellentesque ante euismod sed. Aliquam nunc nisi, blandit ac ipsum at, feugiat consectetur ipsum. Suspendisse consequat bibendum leo, sed tincidunt mauris aliquet id. Cras rutrum massa vitae elit eleifend tristique. Suspendisse fermentum sollicitudin ornare. Quisque arcu metus, varius quis libero a, vehicula pretium risus. In efficitur tempus tortor, vel consequat dui lacinia porta. Aenean eleifend nunc at volutpat sollicitudin. Ut vehicula lectus vitae leo bibendum, et consectetur nunc tristique. Quisque ut mauris facilisis, mattis diam sed, finibus felis. Suspendisse varius tristique congue. Aenean efficitur libero quis elit pharetra, sit amet hendrerit dolor congue.
   </p>
 </div>
